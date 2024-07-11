e3da404496bcbe94885ad3692757c3dd1564a415
mahdi-sahebi <m.nejadsahebi@live.co.uk>
2024-07-11 06:20:15 +0330

    [TEST]: Test cases are declared

--- test/test_pipe_state_machine.cpp
+++ test/test_pipe_state_machine.cpp
@@ -1,6 +1,41 @@
 #include "gtest/gtest.h"
 
 
+TEST(creation, valid)
+{
+  FAIL();
+}
+
+TEST(start, zero_tasks)
+{
+  FAIL();
+}
+
+TEST(execution, valid)
+{
+  FAIL();
+}
+
+TEST(execution, restart)
+{
+  FAIL();
+}
+
+TEST(execution, restop)
+{
+  FAIL();
+}
+
+TEST(execution, blocking)
+{
+  FAIL();
+}
+
+TEST(execution, async)
+{
+  FAIL();
+}
+
 int main()
 {
    testing::InitGoogleTest();
